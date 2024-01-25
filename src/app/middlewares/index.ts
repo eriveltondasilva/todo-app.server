8c0f5588b801b50d2e4af86baf0f648c793fee41
Erivelton Silva <eriveltondasilva13@gmail.com>
2024-01-25 09:16:53 -0300

    cors open

--- src/app/middlewares/index.ts
+++ src/app/middlewares/index.ts
@@ -1,4 +1,4 @@
-import cors, { CorsOptions } from 'cors'
+import cors from 'cors'
 import { Express, json } from 'express'
 
 /**
@@ -6,13 +6,9 @@ import { Express, json } from 'express'
  * @desc A class that contains middlewares for the server
  **/
 class Middlewares {
-  private static corsOptions: CorsOptions = {
-    origin: '*',
-  }
-
   /** @desc Adds middlewares to the Express application **/
   static use(App: Express): void {
-    App.use(cors(this.corsOptions))
+    App.use(cors())
     App.use(json())
   }
 }
