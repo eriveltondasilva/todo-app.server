993087ccb9ffc5c6dfa5ac3d2c3d5f65a01569e6
Erivelton Silva <eriveltondasilva13@gmail.com>
2024-01-25 08:54:58 -0300

    api

--- dist/app/config/setting.js
+++ dist/app/config/setting.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.APP_HOST = exports.APP_PORT = void 0;
 exports.APP_PORT = Number(process.env.APP_PORT) || 3000;
-exports.APP_HOST = process.env.APP_HOST || '';
+exports.APP_HOST = process.env.APP_HOST || 'https://todo-app-client-eriveltonsilva.vercel.app:3000/';
