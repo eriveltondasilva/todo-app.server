aeb86aa86dff16b7a00cf763f6c223ffbca446f7
Erivelton Silva <eriveltondasilva13@gmail.com>
2024-01-25 08:49:47 -0300

    api

--- src/app/config/setting.ts
+++ src/app/config/setting.ts
@@ -1,2 +1,2 @@
 export const APP_PORT = Number(process.env.APP_PORT) || 3000
-export const APP_HOST = process.env.APP_HOST || ''
+export const APP_HOST = process.env.APP_HOST || 'https://todo-app-client-eriveltonsilva.vercel.app/'
