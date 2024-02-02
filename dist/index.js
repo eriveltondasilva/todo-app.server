143270eebb42a82b63e0c93d8a2873a4ef71911a
Erivleton Silva <eriveltondasilva13@gmail.com>
2024-02-02 03:47:26 -0300

    build

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
 const middlewares_1 = __importDefault(require("./app/middlewares"));
 const routes_1 = __importDefault(require("./routes"));
