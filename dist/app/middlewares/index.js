f1db500f3c8d5f12c2634acbb79ecc571462972b
Erivleton Silva <eriveltondasilva13@gmail.com>
2024-02-02 18:02:25 -0300

    build: adiciona rate-limit

--- dist/app/middlewares/index.js
+++ dist/app/middlewares/index.js
@@ -1,9 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const express_rate_limit_1 = require("express-rate-limit");
+const limit = (0, express_rate_limit_1.rateLimit)({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
 class Middlewares {
     static use(App) {
         App.use((0, express_1.json)());
+        App.use(limit);
     }
 }
 exports.default = Middlewares;
--- src/app/middlewares/index.ts
+++ src/app/middlewares/index.ts
@@ -1,20 +1,30 @@
+//# GLOBAL MIDDLEWARES
 import { Express, json } from 'express'
+import { rateLimit } from 'express-rate-limit'
+
+// ====================================
 
 // import { APP_HOST } from '@/config/constants'
-
 // const corsOptions = {
 //   origin: APP_HOST,
 // }
 
+const limit = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 /**
  * @class Middlewares
- * @desc A class that contains middlewares for the server
- **/
+ * @desc A class that contains global middlewares
+ */
 class Middlewares {
-  /** @desc Adds middlewares to the Express application **/
   static use(App: Express): void {
-    // App.use(cors(corsOptions))
     App.use(json())
+    // App.use(cors(corsOptions))
+    App.use(limit)
   }
 }
 
