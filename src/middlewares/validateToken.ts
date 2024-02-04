570244f7b7e570e59efedfe98a02d895e4954153
Erivleton Silva <eriveltondasilva13@gmail.com>
2024-02-03 21:43:22 -0300

    build: realoca middlewares

--- src/middlewares/validateToken.ts
+++ src/middlewares/validateToken.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 
-import { SECRET_JWT } from '../config/constants'
+import { SECRET_JWT } from '@/config/constants'
 
 // =====================================
 export default function validateToken(req: Request, res: Response, next: NextFunction) {
