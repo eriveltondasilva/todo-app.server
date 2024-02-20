7f166bc5da18f007e6c2187f635a09a920668528
Erivleton Silva <eriveltondasilva13@gmail.com>
2024-02-20 06:18:55 -0300

    conf: desativa cors

--- dist/middlewares/@index.mw.js
+++ dist/middlewares/@index.mw.js
@@ -11,11 +11,6 @@ const express_rate_limit_1 = require("express-rate-limit");
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const env_config_1 = require("../app/config/env.config");
-const corsOptions = {
-    origin: env_config_1.APP_HOST,
-    credentials: true,
-    maxAge: 15 * 60 * 1000,
-};
 const rateLimitOptions = {
     windowMs: 15 * 60 * 1000,
     limit: 100,
@@ -29,7 +24,7 @@ class Middlewares {
         app.use((0, compression_1.default)());
         app.use((0, express_1.json)());
         app.use((0, helmet_1.default)());
-        app.use((0, cors_1.default)(corsOptions));
+        app.use((0, cors_1.default)());
         app.use((0, cookie_parser_1.default)(env_config_1.COOKIE_PARSER_SECRET));
         app.use((0, morgan_1.default)(morganOptions));
         app.use((0, express_rate_limit_1.rateLimit)(rateLimitOptions));
